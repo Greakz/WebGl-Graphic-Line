c7c45ee0bbaf080dfa3417e357c33da287653e63
Luke <luke.thienemann@icloud.com>
2019-03-20 10:58:12 +0100

    fixed transparency blending with skybox

--- Shader/FinalizeLightningShader.glsl
+++ Shader/FinalizeLightningShader.glsl
@@ -21,6 +21,7 @@ uniform sampler2D brightness_result;
 
 uniform sampler2D position_map;
 uniform sampler2D t_material_map;
+uniform sampler2D t_transparency_map;
 
 uniform sampler2D skybox;
 // uniform sampler2D codiertemapblamitstencilinfos;
@@ -31,14 +32,20 @@ layout(location = 0) out vec4 outColor;
 void main(void) {
     vec3 scene_pixel = texture(scene_result, vTex).rgb;
     float pixel_depth = texture(position_map, vTex).w;
+    float t_transparency = texture(t_transparency_map, vTex).r;
     vec3 brightness_pixel = texture(brightness_result, vTex).rgb;
 
     vec3 combine_pixel = vec3(0.0);
 
+    outColor = vec4(vec3(t_transparency), 1.0);
+
     if(pixel_depth < 1.0) {
         // use Scene Pixel
         combine_pixel = scene_pixel;
-    }  else {
+    }  else if (t_transparency > 0.0) {
+        vec3 skymap_pixel = texture(skybox, vTex).rgb;
+        combine_pixel = (scene_pixel * vec3(t_transparency)) + (skymap_pixel * vec3(1.0 - t_transparency));
+    } else {
         // use Skybox Pixel
         vec3 skymap_pixel = texture(skybox, vTex).rgb;
         combine_pixel = skymap_pixel;
@@ -46,4 +53,5 @@ void main(void) {
     //vec3 combine_pixel = (getAddedComponents(light_bulb_pixel) == 0) ? light_calc_pixel : light_bulb_pixel;
     combine_pixel = min(max(combine_pixel, vec3(0.0)) + max(brightness_pixel, vec3(0.0)), 1.0);
     outColor = vec4(combine_pixel, 1.0);
+
 }
--- src/Core/Controller/PrivateController/RenderPass/LightningPass/LightningPassFinalize.ts
+++ src/Core/Controller/PrivateController/RenderPass/LightningPass/LightningPassFinalize.ts
@@ -21,6 +21,8 @@ export abstract class LightningPassFinalize {
         GL.activeTexture(GL.TEXTURE2);
         GL.bindTexture(GL.TEXTURE_2D, GeometryPass.solid_storage.position_texture);
         GL.activeTexture(GL.TEXTURE3);
+        GL.bindTexture(GL.TEXTURE_2D, TransparencyPass.transparent_storage.blend_transparency_texture);
+        GL.activeTexture(GL.TEXTURE4);
         GL.bindTexture(GL.TEXTURE_2D, SkyboxPass.screen_gen_result);
 
         GL.drawArrays(GL.TRIANGLES, 0, 6);
